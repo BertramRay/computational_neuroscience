4d2b8537838a02737668290b1e70a38ef6dbffdd
leiziyang(bertram) <1034495527@qq.com>
2022-03-06 19:31:32 +0800

    homework_1_2

--- hw1_2.py
+++ hw1_2.py
@@ -6,8 +6,8 @@ import seaborn as sns
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    m1 = loadmat("E:\【学习】\系统与计算神经科学\作业\SCNS2022_HW1\Auditory Neuron\R15N111_Spikes.mat")
-    m2 = loadmat("E:\【学习】\系统与计算神经科学\作业\SCNS2022_HW1\Auditory Neuron\R15N111_Stimulus.mat")
+    m1 = loadmat("data/Auditory Neuron/R15N111_Spikes.mat")
+    m2 = loadmat("data/Auditory Neuron/R15N111_Stimulus.mat")
     unit1 = m1["unit1"]
     unit2 = m1["unit2"]
     freqs = m2["freqs"]
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     u1len = unit1.shape[0]
     u2len = unit2.shape[0]
     sti_len = sti_onset.shape[0]
+
     for i in range(u1len):
         spike_time = unit1[i][0]
         for j in range(sti_len):
@@ -42,13 +43,20 @@ if __name__ == '__main__':
                 # print(level_idx, freq_idx)
                 mat2[level_idx][freq_idx] += 1
 
-    data1 = np.array(mat1)
-    data2 = np.array(mat2)
-
     f, ax = plt.subplots(figsize=(9, 6))
-    data1 = pd.DataFrame(data1, columns=np.arange(20, 62, 2), index=np.arange(0, 45, 5))
-    ax = sns.heatmap(data1)
     ax.set_title("Frequency Response Area(Unit 1)")
     ax.set_xlabel("Freq (kHz)")
     ax.set_ylabel("dB SPL")
-    plt.show()
+    data1 = np.array(mat1)
+    data1 = pd.DataFrame(data1, columns=np.arange(20, 62, 2), index=np.arange(0, 45, 5))
+    ax = sns.heatmap(data1)
+    plt.savefig("result/unit_1_result.png")
+
+    f, ax = plt.subplots(figsize=(9, 6))
+    ax.set_title("Frequency Response Area(Unit 2)")
+    ax.set_xlabel("Freq (kHz)")
+    ax.set_ylabel("dB SPL")
+    data2 = np.array(mat2)
+    data2 = pd.DataFrame(data2, columns=np.arange(20, 62, 2), index=np.arange(0, 45, 5))
+    ax = sns.heatmap(data2)
+    plt.savefig("result/unit_2_result.png")
